2461b52e9436daa886794d7d84262b584c844817
Huynh Ba Anh <huynhbaanh311@gmail.com>
2026-06-13 14:45:45 +0700

    set up env fe

--- legal-disclosure-react/src/api/client.ts
+++ legal-disclosure-react/src/api/client.ts
@@ -1,13 +1,11 @@
 import axios from "axios";
 
 const client = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: import.meta.env.VITE_API_BASE_URL || import.meta.env.VITE_API_URL_RENDER,
     headers: {
         "Content-Type": "application/json",
     },
 });
-
-console.log(import.meta.env.VITE_API_BASE_URL);
 
 client.interceptors.request.use(
     (config) => {
