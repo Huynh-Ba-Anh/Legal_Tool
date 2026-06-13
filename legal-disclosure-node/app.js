210997760762ece68b75e342afde1e24ba780d55
Huynh Ba Anh <huynhbaanh311@gmail.com>
2026-06-13 16:34:37 +0700

    chore: fix vulnerabilities and update dependencies

--- legal-disclosure-node/app.js
+++ legal-disclosure-node/app.js
@@ -21,9 +21,6 @@ var app = express();
 
 connectDb();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
