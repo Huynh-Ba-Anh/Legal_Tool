2048d57888b6fc8e3a06955b4153d3f00f4570f4
Huynh Ba Anh <huynhbaanh311@gmail.com>
2026-06-16 00:25:33 +0700

    set up .env

--- legal-disclosure-react/src/api/client.ts
+++ legal-disclosure-react/src/api/client.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log("API URL =", import.meta.env.VITE_API_BASE_URL);
+
 const client = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
