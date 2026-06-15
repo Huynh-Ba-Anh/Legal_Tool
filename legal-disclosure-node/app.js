cd00fdc09a5b7750bef2b9984a037751b3bd95aa
Huynh Ba Anh <huynhbaanh311@gmail.com>
2026-06-15 23:58:44 +0700

    fix app.js

--- legal-disclosure-node/app.js
+++ legal-disclosure-node/app.js
@@ -21,9 +21,6 @@ var app = express();
 
 connectDb();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -50,11 +47,12 @@ app.use(function (req, res, next) {
 });
 
 app.use(function (err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  console.error(err);
 
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message,
+  });
 });
 
 module.exports = app;
