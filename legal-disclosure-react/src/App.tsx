c111be3b38ba7e8852d5b868bc69581f9a3a11d8
Huynh Ba Anh <huynhbaanh311@gmail.com>
2026-06-18 17:21:21 +0700

    fix fe scroll

--- legal-disclosure-react/src/App.tsx
+++ legal-disclosure-react/src/App.tsx
@@ -1,21 +1,23 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import AdminLogin from './pages/AdminLogin'
-import Dashboard from './pages/Dashboard'
-import ProtectedRoute from './components/ProtectedRoute'
-import Footer from './components/HomePage/Footer'
-import Header from './components/HomePage/Header'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AdminLogin from "./pages/AdminLogin";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Footer from "./components/HomePage/Footer";
+import Header from "./components/HomePage/Header";
+import ScrollToTop from "./components/ui/ScrollToTop";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
+
       <Header />
+
       <Routes>
-        {/* PUBLIC ROUTES  */}
         <Route path="/" element={<HomePage />} />
         <Route path="/admin-login" element={<AdminLogin />} />
 
-        {/* PRIVATE ROUTES */}
         <Route element={<ProtectedRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
         </Route>
@@ -23,5 +25,5 @@ export default function App() {
 
       <Footer />
     </BrowserRouter>
-  )
+  );
 }
